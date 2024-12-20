ceba676d254667dde0b9419308d252a71f8ed31b
maker2413 <maker2413@gmail.com>
2024-12-19 22:03:50 -0600

    trying to get user input reading working

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gosh
 
 go 1.23.4
+
+require (
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
+
+	"golang.org/x/term"
 )
 
-func main() {
+func rewrite() {
 	fmt.Printf("%d", 1000000000)
 	time.Sleep(1 * time.Second)
 	for i := 10; i >= 0; i-- {
@@ -14,4 +18,78 @@ func main() {
 		fmt.Printf("%d", i)
 	}
 	fmt.Println()
+}
+
+func getKey() []byte {
+	// Get the input file descriptor
+	fd := int(os.Stdin.Fd())
+
+	// Save the original terminal state to restore later
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(fd, oldState)
+
+	// Create a new terminal instance
+	//term := term.NewTerminal(os.Stdin, "")
+
+	b := make([]byte, 3)
+	_, err = os.Stdin.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+
+	// for {
+	// 	// Read a single byte
+	// 	b, err := term.ReadByte()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	switch b {
+	// 	case 27: // ESC
+	// 		b2, _ := term.ReadByte()
+	// 		if b2 == 91 { // '['
+	// 			b3, _ := term.ReadByte()
+	// 			switch b3 {
+	// 			case 65:
+	// 				fmt.Println("Up arrow")
+	// 			case 66:
+	// 				fmt.Println("Down arrow")
+	// 			case 67:
+	// 				fmt.Println("Right arrow")
+	// 			case 68:
+	// 				fmt.Println("Left arrow")
+	// 			}
+	// 		}
+	// 	default:
+	// 		fmt.Printf("Key pressed: %v\n", b)
+	// 	}
+	// }
+}
+
+func readUserInput() {
+	var line strings.Builder
+	fmt.Print(":> ")
+
+	for {
+		k := getKey()
+		if k[0] == byte(13) {
+			fmt.Println(line)
+			break
+		} else {
+			fmt.Print(string(k))
+			line.WriteString(string(k))
+		}
+	}
+}
+
+func main() {
+	readUserInput()
+
+	// rewrite()
+
 }
